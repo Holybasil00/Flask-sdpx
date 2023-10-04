7beccb9b9106e43c1cf35a5cb8df26f6e560be7a
Holybasil00 <88477383+Holybasil00@users.noreply.github.com>
2023-10-05 01:10:01 +0700

    add unit_tset

---
+++ unit_test.py
@@ -0,0 +1,11 @@
+#unit test
+import unittest
+import app
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_test(self):
+        self.assertEqual(app.test(), "test")
+
+if __name__ == '__main__':
+    unittest.main()
